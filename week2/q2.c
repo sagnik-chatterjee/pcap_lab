e6eef771a43f8c6e64a65a0b37b74faf204dda57
Sagnik Chatterjee <chatterjeesagnik88@gmail.com>
2021-03-02 14:45:14 +0530

    added files

--- week2/q2.c
+++ week2/q2.c
@@ -4,36 +4,46 @@
 
 #define SIZE sizeof(int)
 
-int main (int argc, char *argv []) {
+int main (int argc, char *argv [])
+{
 
-	int size, rank;
-	MPI_Status status;
+    int size, rank;
+    MPI_Status status;
 
-	MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
 
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int *number = (int *)malloc(SIZE);
-	
+    int *number = (int *)malloc(SIZE);
 
-	if (rank == 0) {
+    for(int i = 0; i < SIZE; i++)
+    {
+        //fill up the number array with random numbers
+        number[i] =  rand() % ((i + 1) * 100) + 1;
+    }
 
-		*number = rand() % 10 + 1;
-		for (int i = 1; i < size; ++i) {
-			printf("The rank :- %d. Sent to %d:- %d\n", rank, i, *number);
-			// Send to the process with ID = i
-			MPI_Send(number, SIZE, MPI_INT, i, 100 + i, MPI_COMM_WORLD);
-		}
+    if (rank == 0)
+    {
 
-	} else {
-		
-		// Revc from the process with ID = 0
-		MPI_Recv(number, SIZE, MPI_INT, 0, 100 + rank, MPI_COMM_WORLD, &status);
+        for (int i = 1; i < size; ++i)
+        {
+            printf("Sending rank :- %d. Sent to proces of rank %d:- %d\n", rank, i, *number);
+            // Send to the process with ID = i
+            MPI_Send(number, SIZE, MPI_INT, i,  i, MPI_COMM_WORLD);
+        }
 
-		printf("The rank:- %d. Received:-  %d\n", rank, *number);
+    }
+    else
+    {
 
-	}
+        // Revc from the process with ID = 0
+        MPI_Recv(number, SIZE, MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
 
-	MPI_Finalize();
+        printf("The receiver rank:- %d. Received:-  %d\n", rank, *number);
+
+    }
+
+    MPI_Finalize();
+    return 0;
 }
